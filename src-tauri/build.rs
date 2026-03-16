1a111e1991f60e3763f3d303fa2f49c431f6b7a3
Jufeng Zhang <zjffun@gmail.com>
2026-03-17 01:49:07 +0800

    fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,3 @@
 fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        let mut res = winres::WindowsResource::new();
-        res.set_manifest_file("app.manifest");
-        res.compile().expect("Failed to compile Windows resources");
-    }
     tauri_build::build()
 }
